41189ec8c07273487a9582457412fcb67f7b9363
James Ly <jamesquietly@gmail.com>
2025-09-27 18:06:23 -0700

    static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
 };
 
 export default nextConfig;
